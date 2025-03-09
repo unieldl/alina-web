db58cef81e8b08e6905ae7b5cdfd1377bbf7ca8e
unieldl <unieldl@yandex.com>
2025-03-09 19:49:03 +0300

    Add files via upload

---
+++ script/modal.js
@@ -0,0 +1,16 @@
+document.getElementById('open_modal_heart_button').addEventListener('click',function(){
+    document.getElementById('modal_container_heart').classList.add('open')
+})
+
+document.getElementById('close_button_heart').addEventListener('click',function(){
+    document.getElementById('modal_container_heart').classList.remove('open')
+})
+
+
+document.getElementById('open_modal_star_button').addEventListener('click',function(){
+    document.getElementById('modal_container_star').classList.add('open')
+})
+
+document.getElementById('close_button_star').addEventListener('click',function(){
+    document.getElementById('modal_container_star').classList.remove('open')
+})
